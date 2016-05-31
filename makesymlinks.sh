cd2d0305dd7990578467fa6cca37675fc61f356a
mbader22 <mbader2@gmx.at>
2016-05-31 17:15:13 +0200

    gimp scripts hinzugefügt

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 ############################
-# makesl.sh
+# makesymlinks.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 files=('zsh/aliases.zsh' 'zsh/functions.zsh' 'bashrc' 'vimrc' 'zshrc' 'config/Thunar/uca.xml' 'dircolors' 'config/xfce4/terminal/terminalrc')    # list of files/folders to symlink in homedir
-
+folders=('gimp-2.8/scripts')
 ##########
 
 # create dotfiles_old in homedir
@@ -25,9 +25,20 @@ echo "...done\n"
 # create symlinks
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
+  mv ~/.$file $olddir/
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
+done
+
+# create symlinks
+for folder in $folders; do
+  if mkdir $olddir/${folder%/*} | grep 'kein Verzeichnis'
+  then
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$folder/ $olddir/$folder
+  fi
+  echo "Creating symlink to $folder in home directory."
+  ln -s -r $dir/$folder/ ~/.${folder%/*}/
 done
 
 echo "...done\n"
