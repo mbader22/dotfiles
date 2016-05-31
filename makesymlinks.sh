dc083865e3b61e3423fc71ced0443588028b716f
mbader22 <mbader2@gmx.at>
2016-05-31 20:28:52 +0200

    makesymlink.sh: Texthighlighting hinzugefügt

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,6 +4,13 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+error="[\e[31merror\e[0m"
+move="[\e[32mmove\e[0m"
+delete="[\e[32mdelete\e[0m"
+create="[\e[32mcreate\e[0m"
+idle="[\e[32midle\e[0m]"
+highlight="\e[97m"
+default="\e[0m"
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
@@ -13,8 +20,13 @@ folders=('gimp-2.8/scripts')
 
 # create dotfiles_old in homedir
 olddir=~/dotfiles_old             # old dotfiles backup directory
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+  if [[ ! -L ~/.$file ]]
+  then
+    echo "$create $highlight$olddir$default] Creating for backup of any existing dotfiles in ~"
+  else
+    echo "$idle Folder $highlight$olddir$default for backup already exists"
+  fi
+  mkdir -p $olddir
 echo "...done\n"
 
 # change to the dotfiles directory
@@ -24,21 +36,47 @@ echo "...done\n"
 
 # create symlinks
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file $olddir/
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+  if [[ ! -L ~/.$file ]]
+  then
+    echo "$move $highlight~/.$file$default] Move from ~ to $olddir"
+    mv ~/.$file $olddir/
+  else
+    echo "$delete $highlight~/.$file$default] is a symlink and will be deletet"
+    rm ~/.$file
+  fi
+    echo "$create $highlight~/.$file$default] Creating symlink in home directory"
+    ln -s $dir/$file ~/.$file
 done
 
 # create symlinks
 for folder in $folders; do
-  if mkdir $olddir/${folder%/*} | grep 'kein Verzeichnis'
+  if [[ ! -d $olddir/${folder%/*} ]]
   then
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$folder/ $olddir/$folder
+    echo "$create $highlight~/.$folder/$default] Creating Directory"
+    mkdir $olddir/${folder%/*}
+  else
+    echo "$idle Directory $highlight~/.$folder/$default already exists"
   fi
-  echo "Creating symlink to $folder in home directory."
-  ln -s -r $dir/$folder/ ~/.${folder%/*}/
+  if [[ ! -L ~/.$folder ]]
+  then
+    if [[ -e ~/.$folder ]]
+    then
+      if [[ -d ~/.$folder ]]
+      then
+        echo "$move $highlight~/.$folder$default] Move from ~ to $olddir"
+        mv ~/.$folder/ $olddir/$folder
+        echo "$create $highlight$folder$default] Creating Symlink in home directory"
+        ln -s -r $dir/$folder/ ~/.${folder%/*}/
+      else
+        echo "$error $highlight$folder$default] file already exists but its not a Folder!"
+      fi
+    else
+        echo "$create $highlight$folder$default] Creating Symlink in home directory"
+        ln -s -r $dir/$folder/ ~/.${folder%/*}/
+    fi
+  else
+    echo "$idle symlink to $highlight$folder$default already exists"
+  fi
 done
 
 echo "...done\n"
