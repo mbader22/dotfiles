b1dd2b5ed71c08bbffb0d7023cc594430215db49
mbader22 <mbader2@gmx.at>
2016-06-15 22:04:53 +0200

    Liste der zu installierenden Programme angepasst

--- fullsetup.sh
+++ fullsetup.sh
@@ -2,6 +2,8 @@
 
 ~/dotfiles/makesymlinks.sh
 
-pacaur -S --needed vim-nerdtree vim-ctrlp vim-airline vim-airline-themes nerd-fonts-git vim-kolor zsh vim-wombat oh-my-zsh-git
+pacaur -S --needed vim-nerdtree vim-ctrlp vim-airline vim-airline-themes nerd-fonts-git zsh vim-wombat oh-my-zsh-git
 
-pacaur -S --needed gimp inkscape google-chrome thunderbird xfce4-whiskermenu-plugin xfce4-volumed octave libreoffice-still libreoffice-still-de pdfshuffler simple-scan gutenprint audacious meld
+pacaur -S --needed gimp inkscape google-chrome thunderbird xfce4-whiskermenu-plugin xfce4-volumed octave libreoffice-still libreoffice-still-de pdfshuffler simple-scan gutenprint audacious meld gtk-theme-arc numix-circle-icon-theme-git evince-no-gnome grive mediathek vlc mplayer gparted lightdm-gtk-greeter lightdm-gtk-greeter-settings
+
+pacaur -R pidgin
