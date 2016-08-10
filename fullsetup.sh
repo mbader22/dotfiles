b77bc2726a48cc5a5c176fcfeb649a085fef9c67
mbader22 <mbader2@gmx.at>
2016-08-10 18:36:38 +0200

    kleine Änderungen

--- fullsetup.sh
+++ fullsetup.sh
@@ -5,7 +5,8 @@
 echo  "software installieren/deinstallieren (j/N)?"
 read response
 
-if [ "$response" == "j" ]; then
+if [ "$response" = "j" ]; then
+
   pacaur -S --needed --noedit --noconfirm vim-nerdtree vim-ctrlp vim-airline vim-airline-themes nerd-fonts-git zsh vim-wombat oh-my-zsh-git
 
   pacaur -S --needed --noedit --noconfirm gimp inkscape google-chrome thunderbird xfce4-whiskermenu-plugin xfce4-volumed octave libreoffice-still libreoffice-still-de pdfshuffler-git simple-scan gutenprint audacious meld gtk-theme-arc numix-circle-icon-theme-git evince-no-gnome grive mediathek vlc mplayer gparted lightdm-gtk-greeter lightdm-gtk-greeter-settings
