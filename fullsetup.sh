567bb8281c0ab8ef9978231f70e07acc02da9566
mbader22 <mbader2@gmx.at>
2016-11-17 13:08:06 +0100

    Programmierstil von makesymlink geändert

--- fullsetup.sh
+++ fullsetup.sh
@@ -1,15 +1,19 @@
 #!/bin/zsh
 
-~/dotfiles/makesymlinks.sh
+source ~/dotfiles/makesymlinks.sh
+makesymlinks
+if [ $? = 0 ] ; then
+  echo  "software installieren/deinstallieren (j/N)?"
+  read -k 1 response
 
-echo  "software installieren/deinstallieren (j/N)?"
-read -k 1 response
+  if [ "$response" = "j" ]; then
 
-if [ "$response" = "j" ]; then
+    pacaur -S --needed --noedit --noconfirm vim-nerdtree vim-ctrlp vim-airline vim-airline-themes nerd-fonts-git zsh vim-wombat oh-my-zsh-git
 
-  pacaur -S --needed --noedit --noconfirm vim-nerdtree vim-ctrlp vim-airline vim-airline-themes nerd-fonts-git zsh vim-wombat oh-my-zsh-git
+    pacaur -S --needed --noedit --noconfirm gimp inkscape google-chrome thunderbird xfce4-whiskermenu-plugin xfce4-volumed octave libreoffice-still libreoffice-still-de pdfshuffler-git simple-scan gutenprint audacious meld gtk-theme-arc numix-circle-icon-theme-git evince-no-gnome grive mediathek vlc mplayer gparted lightdm-gtk-greeter lightdm-gtk-greeter-settings
 
-  pacaur -S --needed --noedit --noconfirm gimp inkscape google-chrome thunderbird xfce4-whiskermenu-plugin xfce4-volumed octave libreoffice-still libreoffice-still-de pdfshuffler-git simple-scan gutenprint audacious meld gtk-theme-arc numix-circle-icon-theme-git evince-no-gnome grive mediathek vlc mplayer gparted lightdm-gtk-greeter lightdm-gtk-greeter-settings
-
-  pacaur -R pidgin
+    pacaur -R pidgin
+  fi
+else
+  echo "konnte Symlinks nicht erzeugen!"
 fi
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -33,95 +33,96 @@ readonly MS_OLDDIR=~/dotfiles_old             # old dotfiles backup directory
 
 
 
-
-if [[ ! -d $MS_OLDDIR ]] ; then
-  if [[ ! -a $MS_OLDDIR ]] ; then
-    echo "$MS_CREATE $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] Creating for backup of any existing dotfiles in ~"
-    mkdir -p $MS_OLDDIR
-    echo "...done\n"
+function makesymlinks(){
+  if [[ ! -d $MS_OLDDIR ]] ; then
+    if [[ ! -a $MS_OLDDIR ]] ; then
+      echo "$MS_CREATE $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] Creating for backup of any existing dotfiles in ~"
+      mkdir -p $MS_OLDDIR
+      echo "...done\n"
+    else
+      echo "$MS_ERROR $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT  already exists and is not an directory"
+      echo "$MS_ERROR $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] rename or remove file $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT before starting this symlink script"
+      echo "$MS_ERROR] exiting without symlinking the dotfiles"
+      exit 1
+    fi
   else
-    echo "$MS_ERROR $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT  already exists and is not an directory"
-    echo "$MS_ERROR $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] rename or remove file $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT before starting this symlink script"
-    echo "$MS_ERROR] exiting without symlinking the dotfiles"
-    exit 1
+    echo "$MS_IDLE $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] Directory for backup already exists"
   fi
-else
-  echo "$MS_IDLE $MS_HIGHLIGHT$MS_OLDDIR$MS_DEFAULT] Directory for backup already exists"
-fi
 
-# change to the dotfiles directory
-#echo "Changing to the $MS_DIR directory"
-#cd $MS_DIR
-#echo "...done\n"
+  # change to the dotfiles directory
+  #echo "Changing to the $MS_DIR directory"
+  #cd $MS_DIR
+  #echo "...done\n"
 
-# create symlinks
-for file in $MS_FILES; do
-  if [[ -e ~/.$file ]] ; then
-    if [[ ! -L ~/.$file ]] ; then
-      if [[ -f ~/$file ]] ; then
-        echo "$move $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Move from ~ to $MS_OLDDIR"
-        mv ~/.$file $MS_OLDDIR/
-        echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
-        mkdir -p ~/.${file%/*}
-        ln -s -f $MS_DIR/$file ~/.$file
-      else
-        if [[ -d ~/$file ]] ; then
-          echo "$MS_ERROR $MS_HIGHLIGHT~/.$file$MS_DEFAULT] ~/.$file is a directory and not a file. Can't make a symlink"
+  # create symlinks
+  for file in $MS_FILES; do
+    if [[ -e ~/.$file ]] ; then
+      if [[ ! -L ~/.$file ]] ; then
+        if [[ -f ~/$file ]] ; then
+          echo "$move $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Move from ~ to $MS_OLDDIR"
+          mv ~/.$file $MS_OLDDIR/
+          echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
+          mkdir -p ~/.${file%/*}
+          ln -s -f $MS_DIR/$file ~/.$file
         else
-          if [[ ${file##*.} -eq $MS_ENDING ]] ; then
-            echo "$move $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Move from ~ to $MS_OLDDIR"
-            mv ~/.$file $MS_OLDDIR/
-            echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
-            mkdir -p ~/.${file%/*}
-            ln -s -f $MS_DIR/$file ~/.$file
+          if [[ -d ~/$file ]] ; then
+            echo "$MS_ERROR $MS_HIGHLIGHT~/.$file$MS_DEFAULT] ~/.$file is a directory and not a file. Can't make a symlink"
           else
-            echo "$MS_ERROR $MS_HIGHLIGHT~/.$file$MS_DEFAULT] ~/.$file is not a symlink, or file, or directory. Can't make a Symlink"
+            if [[ ${file##*.} -eq $MS_ENDING ]] ; then
+              echo "$move $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Move from ~ to $MS_OLDDIR"
+              mv ~/.$file $MS_OLDDIR/
+              echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
+              mkdir -p ~/.${file%/*}
+              ln -s -f $MS_DIR/$file ~/.$file
+            else
+              echo "$MS_ERROR $MS_HIGHLIGHT~/.$file$MS_DEFAULT] ~/.$file is not a symlink, or file, or directory. Can't make a Symlink"
+            fi
           fi
         fi
+      else
+        echo "$MS_DELETE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] is a symlink and will be deletet"
+        rm ~/.$file
+        echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home MS_DIRectory"
+        ln -s -f $MS_DIR/$file ~/.$file
       fi
     else
-      echo "$MS_DELETE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] is a symlink and will be deletet"
-      rm ~/.$file
-      echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home MS_DIRectory"
+      echo "$MS_INFO $MS_HIGHLIGHT~/.$file$MS_DEFAULT] file doesnt exist"
+      echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
       ln -s -f $MS_DIR/$file ~/.$file
     fi
-  else
-    echo "$MS_INFO $MS_HIGHLIGHT~/.$file$MS_DEFAULT] file doesnt exist"
-    echo "$MS_CREATE $MS_HIGHLIGHT~/.$file$MS_DEFAULT] Creating symlink in home directory"
-    ln -s -f $MS_DIR/$file ~/.$file
-  fi
-done
+  done
 
-# create symlinks
-for folder in $MS_FOLDERS; do
-  if [[ ! -d $MS_OLDDIR/${folder%/*} ]] ; then
-    echo "$MS_CREATE $MS_HIGHLIGHT~/.$folder/$MS_DEFAULT] Creating Directory"
-    mkdir -p $MS_OLDDIR/${folder%/*}
-  else
-    echo "$MS_IDLE $MS_HIGHLIGHT~/.$folder/$MS_DEFAULT] Directory already exists"
-  fi
-  if [[ ! -L ~/.$folder ]] ; then
-    if [[ -e ~/.$folder ]]
-    then
-      if [[ -d ~/.$folder ]] ; then
-        echo "$MS_MOVE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] Move from ~/.$folder/ to $MS_OLDDIR"
-        mv ~/.$folder/ $MS_OLDDIR/$folder
+  # create symlinks
+  for folder in $MS_FOLDERS; do
+    if [[ ! -d $MS_OLDDIR/${folder%/*} ]] ; then
+      echo "$MS_CREATE $MS_HIGHLIGHT~/.$folder/$MS_DEFAULT] Creating Directory"
+      mkdir -p $MS_OLDDIR/${folder%/*}
+    else
+      echo "$MS_IDLE $MS_HIGHLIGHT~/.$folder/$MS_DEFAULT] Directory already exists"
+    fi
+    if [[ ! -L ~/.$folder ]] ; then
+      if [[ -e ~/.$folder ]]
+      then
+        if [[ -d ~/.$folder ]] ; then
+          echo "$MS_MOVE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] Move from ~/.$folder/ to $MS_OLDDIR"
+          mv ~/.$folder/ $MS_OLDDIR/$folder
+          echo "$MS_CREATE $MS_HIGHLIGHT$folder$MS_DEFAULT] Creating Symlink ~/.${folder%/*}/"
+          ln -s -f -r $MS_DIR/$folder/ ~/.${folder%/*}/
+        else
+          echo "$MS_ERROR $MS_HIGHLIGHT$folder$MS_DEFAULT] file already exists but its not a Folder!"
+        fi
+      else
         echo "$MS_CREATE $MS_HIGHLIGHT$folder$MS_DEFAULT] Creating Symlink ~/.${folder%/*}/"
         ln -s -f -r $MS_DIR/$folder/ ~/.${folder%/*}/
-      else
-        echo "$MS_ERROR $MS_HIGHLIGHT$folder$MS_DEFAULT] file already exists but its not a Folder!"
       fi
     else
-      echo "$MS_CREATE $MS_HIGHLIGHT$folder$MS_DEFAULT] Creating Symlink ~/.${folder%/*}/"
+      echo "$MS_DELETE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] is a symlink and will be deletet"
+      rm -r ~/.$folder
+      echo "$MS_CREATE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] Creating symlink ~/.${folder%/*}/"
       ln -s -f -r $MS_DIR/$folder/ ~/.${folder%/*}/
     fi
-  else
-    echo "$MS_DELETE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] is a symlink and will be deletet"
-    rm -r ~/.$folder
-    echo "$MS_CREATE $MS_HIGHLIGHT~/.$folder$MS_DEFAULT] Creating symlink ~/.${folder%/*}/"
-    ln -s -f -r $MS_DIR/$folder/ ~/.${folder%/*}/
-  fi
-done
+  done
 
-echo "...done"
-exit 0
+  echo "...done"
+  return 0
+}
